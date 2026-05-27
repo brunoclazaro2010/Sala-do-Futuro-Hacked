2b5c3833447b2663e7a89993e1f8c801c5666e76
brunoclazaro2010 <brunoclazaro2010@gmail.com>
2026-05-27 20:53:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 (function(){
   if(document.getElementById("custom-black-menu")) return;
+  
   var menu = document.createElement("div");
   menu.id = "custom-black-menu";
   menu.style.position = "fixed";
@@ -7,10 +8,11 @@
   menu.style.right = "20px";
   menu.style.width = "250px";
   menu.style.height = "300px";
-  menu.style.backgroundColor = "#000000";
+  menu.style.background = "linear-gradient(to bottom, #ff0000, #000000)";
   menu.style.borderRadius = "15px";
   menu.style.zIndex = "999999";
   menu.style.boxShadow = "0 4px 15px rgba(0,0,0,0.3)";
   menu.style.border = "1px solid #333";
+  
   document.body.appendChild(menu);
 })();
