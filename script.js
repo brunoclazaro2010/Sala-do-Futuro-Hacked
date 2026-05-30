e4bbe948eda220aba2c3a0ef2f1dde6c0ad09a98
brunoclazaro2010 <brunoclazaro2010@gmail.com>
2026-05-29 22:01:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-(function(){
+javascript:(function(){
     var id = 'meu-menu-personalizado';
     var menu = document.getElementById(id);
-    
     if (menu) {
         menu.remove();
     } else {
-        menu = document.createElement('div');
+        var menu = document.createElement('div');
         menu.id = id;
         menu.style.position = 'fixed';
         menu.style.top = '10px';
         menu.style.right = '10px';
         menu.style.width = '300px';
-        menu.style.height = '300px';
+        menu.style.height = '400px';
         menu.style.backgroundColor = 'black';
         menu.style.borderRadius = '15px';
         menu.style.zIndex = '999999';
